f065b81f31ca8e7329568bde100762270114b0a0
bibi32 <doneys@orange.fr>
2017-03-28 19:15:42 +0200

    Update ss.py

--- ss.py
+++ ss.py
@@ -1,38 +1,57 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*- 
+
+import MySQLdb
 import urllib2
 import json
+from ConfigParser import SafeConfigParser
 
-# rom = "Sonic The Hedgehog 2 (World).md"
-# crc = "24AB4C3A"
-# romtaille = "1048576"
+config = SafeConfigParser()
+config.read('config.ini')
 
+host = config.get('MYSQL', 'host')
+user = config.get('MYSQL', 'user')
+passwd = config.get('MYSQL', 'passwd')
+db = config.get('MYSQL', 'db')
+
+devid = 'xxx'
+devpassword = 'yyy'
 ssid = 'test'
 sspassword = 'test'
 
-rom = "Sonic The Hedgehog 2 (World).zip"
-romtaille = '749652'
-crc = '50ABC90A'
+db = MySQLdb.connect(host=host, user=user, passwd=passwd, db=db)
+cur = db.cursor()
 
-romnom = rom.replace(" ", "%20").replace("(", "%28").replace(")", "%29")
+consoles = config.items('CONSOLES')
 
+for key, console in consoles:
 
-url = "https://www.screenscraper.fr/api/jeuInfos.php?devid=xxx&devpassword=yyy&softname=zzz&output=json&ssid="+ssid+"&sspassword="+sspassword+"&crc="+crc+"&systemeid=1&romtype=rom&romnom="+romnom+"&romtaille="+romtaille
-req = urllib2.Request(url)
-opener = urllib2.build_opener()
-f = opener.open(req)
+    cur.execute("SELECT * FROM "+console+" WHERE nom_ss is NULL")
+    rows = cur.fetchall()
 
-try:
-    json = json.loads(f.read())
+    for row in rows:
+	rom = row[2]
+	romtaille = row[5]
+	crc  = row[6]
+	romnom = rom.replace(" ", "%20").replace("(", "%28").replace(")", "%29")
 
-    nom_ss =  json["response"]["jeu"]["nom"]
-    system_ss = json["response"]["jeu"]["systemenom"]
+	url = "https://www.screenscraper.fr/api/jeuInfos.php?devid="+devid+"&devpassword="+devpassword+"&softname=zzz&output=json&ssid="+ssid+"&sspassword="+sspassword+"&crc="+crc+"&systemeid=1&romtype=rom&romnom="+romnom+"&romtaille="+romtaille
+	req = urllib2.Request(url)
+	opener = urllib2.build_opener()
+	f = opener.open(req)
 
-    print nom_ss
-    print(nom_ss.upper())
+	try:
+	    fichier_json = json.loads(f.read())
 
-    print system_ss
+	    nom =  fichier_json["response"]["jeu"]["nom"]
+	    system_ss = fichier_json["response"]["jeu"]["systemenom"]
 
-    print url
+	    nom_ss = (nom_ss.upper())
 
-except ValueError, f:
-#    print f
-    print "ERROR"
+	    cur.execute("UPDATE "+console+" SET nom_ss=%s WHERE description=%s ", (nom_ss, rom))
+
+	except ValueError, f:
+	    print "ERROR"
+
+    db.commit()
+db.close()
