b1d2032f3ee3e54d9125c9f7bb72cd506694562e
bibi32 <doneys@orange.fr>
2017-04-01 13:20:17 +0200

    Update clean_sql.py

--- clean_sql.py
+++ clean_sql.py
@@ -26,6 +26,48 @@ cur = db.cursor()
 list = config.items('EXCLUS')
 consoles = config.items('CONSOLES')
 
+
+## Foction count nbr roms null
+def count_null(console):
+    cur.execute("SELECT COUNT(*) FROM "+console+" WHERE status IS NULL")
+    result=cur.fetchone()
+    return str(result[0])
+
+## Fonction count nbr roms OK
+def count_ok(console):
+    sql = "SELECT COUNT(*) FROM "+console+" WHERE status=%s"
+    cur.execute( sql, ('OK',))
+    result=cur.fetchone()
+    return str(result[0])
+
+## Fonction Supprimer doublons
+def suppr_doublon(console):
+    sql = "UPDATE "+console+" SET status=%s WHERE status IS NULL"
+    cur.execute( sql, ('KO',))
+
+## Fonction marquer unique
+def mark_unique(console,colonne):
+    cur.execute("SELECT name, COUNT(*) c FROM "+console+" WHERE status IS NULL GROUP BY "+colonne+" HAVING c = 1 ")
+    rows = cur.fetchall()
+    for row in rows:
+	sql = "UPDATE "+console+" SET status=%s WHERE name=%s"
+	cur.execute( sql, ('OK', row[0]))
+
+## Marquer doublons
+def mark_doublons(console,colonne):
+    cur.execute("SELECT "+colonne+", COUNT(*) c FROM "+console+" WHERE status IS NULL GROUP BY "+colonne+" HAVING c > 1")
+    rows = cur.fetchall()
+    for row in rows:
+	sql = "SELECT name FROM "+console+" WHERE status IS NULL AND "+colonne+"=%s ORDER BY CASE WHEN name LIKE %s THEN 0 WHEN name LIKE %s THEN 1 WHEN name LIKE %s THEN 2 WHEN name LIKE %s THEN 3 ELSE 4 END LIMIT 1"
+	cur.execute( sql, (row[0],'%France%', '%Europe%', '%World%', '%USA%'))
+	rows = cur.fetchall()
+	for row in rows:
+	    sql = "UPDATE "+console+" SET status=%s WHERE name=%s"
+	    cur.execute( sql, ('OK', row[0]))
+
+
+
+
 for key, console in consoles:
     print console
 
@@ -33,21 +75,17 @@ for key, console in consoles:
 #    cur.execute("UPDATE "+console+" SET status = NULL")
 
 ## Count nbr roms
-    cur.execute("SELECT COUNT(*) FROM "+console+" WHERE status IS NULL")
-    result=cur.fetchone()
-    print "nbr roms : " + str(result[0])
+    print "nbr roms : " + count_null(console)
 
 ## Marquer exclus
     for key, variable in list:
 	cur.execute("UPDATE "+console+" SET status=%s WHERE name LIKE %s ",('KO', '%'+variable+'%',))
 
 ## Count nbr roms clean
-    cur.execute("SELECT COUNT(*) FROM "+console+" WHERE status IS NULL")
-    result=cur.fetchone()
-    print "nbr roms clean : " + str(result[0])
+    print "nbr roms clean : " + count_null(console)
 
 ## Mettre a jour noms
-    cur.execute("SELECT * FROM "+console+" WHERE status IS NULL")
+    cur.execute("SELECT * FROM "+console+" WHERE nom IS NULL")
     rows = cur.fetchall()
     for row in rows:
 	description = row[1]
@@ -56,63 +94,26 @@ for key, console in consoles:
 	cur.execute( sql, (nom, description))
 
 ## Marquer unique
-    cur.execute("SELECT name, COUNT(*) c FROM "+console+" WHERE status IS NULL GROUP BY nom HAVING c = 1 ")
-    rows = cur.fetchall()
-    for row in rows:
-	sql = "UPDATE "+console+" SET status=%s WHERE name=%s"
-	cur.execute( sql, ('OK', row[0]))
+    mark_unique(console,'nom')
 
 ## Count nbr roms non unique
-    cur.execute("SELECT COUNT(*) FROM "+console+" WHERE status IS NULL")
-    result=cur.fetchone()
-    print "nbr roms non unique : " + str(result[0])
+    print "nbr roms non unique : " + count_null(console)
 
 ## Marquer doublons
-    cur.execute("SELECT nom, COUNT(*) c FROM "+console+" WHERE status IS NULL GROUP BY nom HAVING c > 1")
-    rows = cur.fetchall()
-    for row in rows:
-	sql = "SELECT name FROM "+console+" WHERE status IS NULL AND nom=%s ORDER BY CASE WHEN name LIKE %s THEN 0 WHEN name LIKE %s THEN 1 WHEN name LIKE %s THEN 2 WHEN name LIKE %s THEN 3 ELSE 4 END LIMIT 1"
-	cur.execute( sql, (row[0],'%France%', '%Europe%', '%World%', '%USA%'))
-	rows = cur.fetchall()
-	for row in rows:
-	    sql = "UPDATE "+console+" SET status=%s WHERE name=%s"
-	    cur.execute( sql, ('OK', row[0]))
+    mark_doublons(console,'nom')
 
 ## Supprimer doublons
-    sql = "UPDATE "+console+" SET status=%s WHERE status IS NULL"
-    cur.execute( sql, ('KO',))
+    suppr_doublon(console)
 
 ## Count nbr roms OK
-    sql = "SELECT COUNT(*) FROM "+console+" WHERE status=%s"
-    cur.execute( sql, ('OK',))
-    result=cur.fetchone()
-    print "nbr roms OK : " + str(result[0])
+    print "nbr roms OK : " + count_ok(console)
 
 ## Count nbr nom_ss manquant
-    sql = "SELECT COUNT(*) FROM "+console+" WHERE status=%s AND nom_ss IS NULL"
-    cur.execute( sql, ('OK',))
-    result=cur.fetchone()
-    print "nbr roms nom_ss manquant : " + str(result[0])
+    print "nbr roms nom_ss manquant : " + count_null(console)
 
-## Copier manquant
-    if result[0] != 0 :
-	directory = missing + console 
-
-	sql = "SELECT * FROM "+console+" WHERE status=%s AND nom_ss IS NULL"
-	cur.execute( sql, ('OK',))
-	rows = cur.fetchall()
-
-	if os.path.exists(directory):
-	    shutil.rmtree(directory)
-
-	for row in rows:
-	    if not os.path.exists(directory):
-		os.makedirs(directory)
-
-	    file_source = source + console + '/' + os.path.splitext(row[2])[0] + '.zip'
-	    file_destination = missing + console + '/' + os.path.splitext(row[2])[0] + '.zip'
-
-	    shutil.copy(file_source, file_destination)
+## Stopper si nom_ss manquant
+    manquant = count_null(console)
+    if int(manquant) != 0 :
 
 	db.commit()
 	db.close()
@@ -124,41 +125,23 @@ for key, console in consoles:
 	cur.execute( sql, ('OK',))
 
 ## Marquer unique nom_ss
-	cur.execute("SELECT name, COUNT(*) c FROM "+console+" WHERE status IS NULL GROUP BY nom_ss HAVING c = 1 ")
-	rows = cur.fetchall()
-	for row in rows:
-	    sql = "UPDATE "+console+" SET status=%s WHERE name=%s"
-	    cur.execute( sql, ('OK', row[0]))
+	mark_unique(console,'nom_ss')
 
 ## Count nbr roms non unique
-	cur.execute("SELECT COUNT(*) FROM "+console+" WHERE status IS NULL")
-	result=cur.fetchone()
-	print "nbr roms non unique : " + str(result[0])
+	print "nbr roms non unique : " + count_null(console)
 
 ## Marquer doublons noms_ss
-	cur.execute("SELECT nom_ss, COUNT(*) c FROM "+console+" WHERE status IS NULL GROUP BY nom_ss HAVING c > 1")
-	rows = cur.fetchall()
-	for row in rows:
-	    sql = "SELECT name FROM "+console+" WHERE status IS NULL AND nom_ss=%s ORDER BY CASE WHEN name LIKE %s THEN 0 WHEN name LIKE %s THEN 1 WHEN name LIKE %s THEN 2 WHEN name LIKE %s THEN 3 ELSE 4 END LIMIT 1"
-	    cur.execute( sql, (row[0],'%France%', '%Europe%', '%World%', '%USA%'))
-	    rows = cur.fetchall()
-	    for row in rows:
-		sql = "UPDATE "+console+" SET status=%s WHERE name=%s"
-		cur.execute( sql, ('OK', row[0]))
+	mark_doublons(console,'nom_ss')
 
 ## Supprimer doublons nom_ss
-	sql = "UPDATE "+console+" SET status=%s WHERE status IS NULL"
-	cur.execute( sql, ('KO',))
+	suppr_doublon(console)
 
 ## Count nbr roms OK
-	sql = "SELECT COUNT(*) FROM "+console+" WHERE status=%s"
-	cur.execute( sql, ('OK',))
-	result=cur.fetchone()
-	print "nbr roms OK : " + str(result[0])
+	print "nbr roms OK : " + count_ok(console)
 
 ## Copier resultat final
 	directory = destination + console
-	sql = "SELECT * FROM "+console+" WHERE status=%s AND nom_ss IS NOT NULL"
+	sql = "SELECT * FROM "+console+" WHERE status=%s"
 	cur.execute( sql, ('OK',))
 	rows = cur.fetchall()
 
@@ -172,7 +155,7 @@ for key, console in consoles:
 	    file_source = source + console + '/' + os.path.splitext(row[2])[0] + '.zip'
 	    file_destination = destination + console + '/' + os.path.splitext(row[2])[0] + '.zip'
 
-	    shutil.copy(file_source, file_destination)
+#	    shutil.copy(file_source, file_destination)
 
 
     db.commit()
