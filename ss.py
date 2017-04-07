5bfe9a6063533a4aaab0c2c100883fabe887f214
bibi32 <doneys@orange.fr>
2017-04-07 23:56:35 +0200

    Update ss.py

--- ss.py
+++ ss.py
@@ -1,28 +1,27 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*- 
 
-import MySQLdb
+import sqlite3
 import urllib
 import urllib2
 import json
 import cgi
+import base64
 from ConfigParser import SafeConfigParser
 
 config = SafeConfigParser()
 config.read('config.ini')
 
-host = config.get('MYSQL', 'host')
-user = config.get('MYSQL', 'user')
-passwd = config.get('MYSQL', 'passwd')
-db = config.get('MYSQL', 'db')
+db = sqlite3.connect('nointro.db')
+cur = db.cursor()
 
 devid = config.get('ID', 'devid')
+devid = base64.b64decode(devid)
 devpassword = config.get('ID', 'devpassword')
+devpassword = base64.b64decode(devpassword)
+
 ssid = config.get('ID', 'ssid')
 sspassword = config.get('ID', 'sspassword')
-
-db = MySQLdb.connect(host=host, user=user, passwd=passwd, db=db)
-cur = db.cursor()
 
 consoles = config.items('CONSOLES')
 
@@ -34,12 +33,13 @@ for key, console in consoles:
     rows = cur.fetchall()
 
     for row in rows:
-	rom = row[2]
-	romtaille = row[5]
-	crc  = row[6]
+	rom = row[1]
+	romtaille = row[4]
+	crc  = row[5]
 	romnom = urllib.quote(rom, safe='')
 
 	url = "https://www.screenscraper.fr/api/jeuInfos.php?devid="+devid+"&devpassword="+devpassword+"&softname=zzz&output=json&ssid="+ssid+"&sspassword="+sspassword+"&crc="+crc+"&systemeid="+systemeid+"&romtype=rom&romnom="+romnom+"&romtaille="+romtaille
+#	url = "https://www.screenscraper.fr/api/jeuInfos.php?devid="+devid+"&devpassword="+devpassword+"&softname=zzz&output=json&crc="+crc
 	req = urllib2.Request(url)
 	opener = urllib2.build_opener()
 	f = opener.open(req)
@@ -50,7 +50,7 @@ for key, console in consoles:
 	    nom_ss =  fichier_json["response"]["jeu"]["nom"]
 	    print nom_ss
 
-	    cur.execute("UPDATE "+console+" SET nom_ss=%s WHERE name=%s ", (nom_ss, rom))
+	    cur.execute("UPDATE "+console+" SET nom_ss=? WHERE name=? ", (nom_ss, rom))
 
 	except ValueError, f:
 	    print "ERROR"
