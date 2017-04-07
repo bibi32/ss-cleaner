e83dd400d6ccb2ad9c2107e22fb183e13e8b3c99
bibi32 <doneys@orange.fr>
2017-04-07 23:56:03 +0200

    Update dat_to_sql.py

--- dat_to_sql.py
+++ dat_to_sql.py
@@ -2,20 +2,15 @@
 # -*- coding: utf-8 -*- 
 
 from lxml import etree
-import MySQLdb
+import sqlite3
 from ConfigParser import SafeConfigParser
 
 config = SafeConfigParser()
 config.read('config.ini')
 
-host = config.get('MYSQL', 'host')
-user = config.get('MYSQL', 'user')
-passwd = config.get('MYSQL', 'passwd')
-db = config.get('MYSQL', 'db')
-
 nointro = config.get('PATH', 'datfilepath')
 
-db = MySQLdb.connect(host=host, user=user, passwd=passwd, db=db)
+db = sqlite3.connect('nointro.db')
 cur = db.cursor()
 
 consoles = config.items('CONSOLES')
@@ -24,10 +19,13 @@ for key, console in consoles:
 
     datfile = nointro + config.get('DAT', console)
 
-    cur.execute("DROP TABLE IF EXISTS %s" % (console))
-    cur.execute("CREATE TABLE IF NOT EXISTS %s ( id INT AUTO_INCREMENT primary key NOT NULL, description varchar(200), name varchar(200), nom varchar(200), nom_ss varchar(200), size varchar(50), crc varchar(50), md5 varchar(50), sha1 varchar(50), exist varchar(5), status varchar(5))" % (console))
+    cur.execute("DROP TABLE IF EXISTS "+console)
+    cur.execute("CREATE TABLE IF NOT EXISTS %s (description varchar(200), name varchar(200), nom varchar(200), nom_ss varchar(200), size varchar(50), crc varchar(50), md5 varchar(50), sha1 varchar(50), exist varchar(5), status varchar(5))" % (console))
+
+    print datfile
 
     tree = etree.parse(datfile)
+    print tree
     for element in tree.iter('game'):
 	description = element.find('description').text
 	name = element.find("rom").attrib['name']
@@ -38,7 +36,7 @@ for key, console in consoles:
 
 	print name
 
-	sql = "INSERT INTO " + console + " (description, name, size, crc, md5, sha1) VALUES (%s, %s, %s, %s, %s, %s)"
+	sql = "INSERT INTO "+ console +" (description, name, size, crc, md5, sha1) VALUES (?, ?, ?, ?, ?, ?)"
 	cur.execute( sql, (description, name, size, crc, md5, sha1))
 
 	db.commit()
